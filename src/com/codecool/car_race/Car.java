541c7504cbe5c3cf069f907c974353222854238e
Aron Gida <ifAronisGida@github.com>
2018-12-21 13:36:47 +0100

    added name generating for Cars

--- src/com/codecool/car_race/Car.java
+++ src/com/codecool/car_race/Car.java
@@ -1,9 +1,37 @@
 package com.codecool.car_race;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Car extends Vehicle {
 
-    public Car (int startSpeed) {
+    private String name;
+
+    Car (int startSpeed) {
         super(startSpeed);
+        name = generateName();
     }
 
+    private String generateName() {
+        ArrayList<String> listOfNames = new ArrayList<>(Arrays.asList("Aurora",
+                "Dusk",
+                "Flux",
+                "Celestial",
+                "Guerilla",
+                "Supremacy",
+                "Morale",
+                "Bullet",
+                "Dragon",
+                "Wish"));
+
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(listOfNames.get(r.nextInt(10)));
+        sb.append(" ");
+        sb.append(listOfNames.get(r.nextInt(10)));
+
+        return sb.toString();
+    }
 }

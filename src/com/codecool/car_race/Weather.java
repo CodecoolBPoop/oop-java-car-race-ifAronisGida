6672460a463cc519e3ee44f17bcd2cd799405df4
Aron Gida <ifAronisGida@github.com>
2018-12-21 12:36:32 +0100

    refactored Weather class

--- src/com/codecool/car_race/Weather.java
+++ src/com/codecool/car_race/Weather.java
@@ -3,10 +3,16 @@ package com.codecool.car_race;
 import java.util.Random;
 
 public class Weather {
-    public boolean setRaining () {
+
+    private boolean isRaining;
+
+    public Weather () {
+        setRaining();
+    }
+
+    public void setRaining () {
         Random r = new Random();
-        boolean isRaining = false;
+        isRaining = false;
         if (r.nextDouble() <= 0.3) isRaining = true;
-        return isRaining;
     }
 }

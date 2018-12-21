197e6fe446ef511298080e5104a5c4813a66aea7
Aron Gida <ifAronisGida@github.com>
2018-12-21 12:42:25 +0100

    added constructor for vehicles class

---
+++ src/com/codecool/car_race/Vehicle.java
@@ -0,0 +1,20 @@
+package com.codecool.car_race;
+
+public class Vehicle {
+
+    private int speed;
+    private int distanceTraveled;
+
+    public Vehicle (int startSpeed) {
+        speed = startSpeed;
+        distanceTraveled = 0;
+    }
+
+    public void prepareForLap(Race race) {
+
+    }
+
+    public void moveForAnHour() {
+
+    }
+}
